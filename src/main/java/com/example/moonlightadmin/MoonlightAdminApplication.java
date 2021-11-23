108429fc6d3e98e1a4c5d449b9bf93110b12ab22
Yian Kim <devyiankim@gmail.com>
2021-11-24 00:02:13 +0900

    init: initial project again

---
+++ src/main/java/com/example/moonlightadmin/MoonlightAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.moonlightadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonlightAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonlightAdminApplication.class, args);
+	}
+
+}
